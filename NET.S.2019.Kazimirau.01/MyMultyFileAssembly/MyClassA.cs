261cb28fdb39ccf18c3ecc3d447c7d0dd4219e73
RyokoAzuno <ryokoazuno@gmail.com>
2019-03-20 17:48:19 +0300

    Add XML comments

--- NET.S.2019.Kazimirau.01/MyMultyFileAssembly/MyClassA.cs
+++ NET.S.2019.Kazimirau.01/MyMultyFileAssembly/MyClassA.cs
@@ -4,12 +4,12 @@ namespace MyMultyFileAssembly
 {
     /// <summary>
     /// A simple class for creating multy-file assembly.
-    /// Use command prompt console: >csc /t:module MyClassA.cs
-    /// to create a netmodule.
-    /// Use command prompt console: >csc /t:library /addmodule:MyClassA.netmodule /addmodule:MyClassB.netmodule /out:MyABMultyFileAssembly.dll
-    /// to create a multi-file assembly.
-    /// Use command prompt console: >csc al /out:MyABCMultiFileAssemblySigned.dll MyClassA.netmodule MyClassA.netmodule MyClassB.netmodule MyClassB.netmodule /keyfile:MyPublicPrivateKey.snk
-    /// to create a signed multi-file assembly.
+    /// Command prompt commnds
+    /// To create a netmodule:                          >csc /t:module MyClassA.cs
+    /// To create a multi-file assembly:                >csc /t:library /addmodule:MyClassA.netmodule /addmodule:MyClassB.netmodule /out:MyABMultyFileAssembly.dll
+    /// To create a signed multi-file assembly:         >csc al /out:MyABCMultiFileAssemblySigned.dll MyClassA.netmodule MyClassA.netmodule MyClassB.netmodule MyClassB.netmodule /keyfile:MyPublicPrivateKey.snk
+    /// To add assembly into GAC (as Administrator):    >gacutil -i MyABCMultiFileAssemblySigned.dll
+    /// To remove assembly from GAC (as Administrator): >gacutil -u MyABCMultiFileAssemblySigned.dll
     /// </summary>
     public class MyClassA
     {
--- NET.S.2019.Kazimirau.01/MyMultyFileAssembly/MyClassC.cs
+++ NET.S.2019.Kazimirau.01/MyMultyFileAssembly/MyClassC.cs
@@ -4,12 +4,12 @@ namespace MyMultyFileAssembly
 {
     /// <summary>
     /// A simple class for creating multy-file assembly
-    /// Use command prompt command: >csc /t:module MyClassC.cs
-    /// to create a netmodule.
-    /// Use command prompt console: >csc /t:library /addmodule:MyClassA.netmodule /addmodule:MyClassB.netmodule /out:MyABMultyFileAssembly.dll
-    /// to create a multi-file assembly.
-    /// Use command prompt console: >csc al /out:MyABCMultiFileAssemblySigned.dll MyClassA.netmodule MyClassA.netmodule MyClassB.netmodule MyClassB.netmodule /keyfile:MyPublicPrivateKey.snk
-    /// to create a signed multi-file assembly.
+    /// Command prompt commnds
+    /// To create a netmodule:                          >csc /t:module MyClassC.cs
+    /// To create a multi-file assembly:                >csc /t:library /addmodule:MyClassA.netmodule /addmodule:MyClassB.netmodule /out:MyABMultyFileAssembly.dll
+    /// To create a signed multi-file assembly:         >csc al /out:MyABCMultiFileAssemblySigned.dll MyClassA.netmodule MyClassA.netmodule MyClassB.netmodule MyClassB.netmodule /keyfile:MyPublicPrivateKey.snk
+    /// To add assembly into GAC (as Administrator):    >gacutil -i MyABCMultiFileAssemblySigned.dll
+    /// To remove assembly from GAC (as Administrator): >gacutil -u MyABCMultiFileAssemblySigned.dll
     /// </summary>
     public class MyClassC
     {
