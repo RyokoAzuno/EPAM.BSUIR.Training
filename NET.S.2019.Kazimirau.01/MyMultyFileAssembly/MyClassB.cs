3eda0a147576a14a1dce989e612795cda35f5eb8
RyokoAzuno <ryokoazuno@gmail.com>
2019-03-20 16:56:59 +0300

    Add a multifile assembly

---
+++ NET.S.2019.Kazimirau.01/MyMultyFileAssembly/MyClassB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyMultyFileAssembly
+{
+    /// <summary>
+    /// A simple class for creating multy-file assembly
+    /// Use command prompt command: >csc /t:module MyClassB.cs
+    /// to create a netmodule.
+    /// Use command prompt console: >csc /t:library /addmodule:MyClassA.netmodule /addmodule:MyClassB.netmodule /out:MyABMultyFileAssembly.dll
+    /// to create a multi-file assembly.
+    /// Use command prompt console: >csc al /out:MyABCMultiFileAssemblySigned.dll MyClassA.netmodule MyClassA.netmodule MyClassB.netmodule MyClassB.netmodule /keyfile:MyPublicPrivateKey.snk
+    /// to create a signed multi-file assembly.
+    /// </summary>
+    public class MyClassB
+    {
+        public void Method() => Console.WriteLine($"{GetType().Name}");
+    }
+}
---
+++ NET.S.2019.Kazimirau.01/MyMultyFileAssemblyClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyMultyFileAssembly;
+using System;
+
+namespace MyMultyFileAssemblyClient
+{
+    /// <summary>
+    /// A client for testing my signed/not multy-file
+    /// assembly (MyABCMultiFileAssembly.dll - not signed ;MyABCMultiFileAssemblySigned.dll - signed).
+    /// To test it, you have to add a reference to MyABCMultiFileAssemblySigned.dll or MyABCMultiFileAssembly.dll(!!!NOT TO A PROJECT!!!)
+    /// from MyMultiFileAssembly project.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClassA a = new MyClassA();
+            a.Method();
+
+            MyClassB b = new MyClassB();
+            b.Method();
+
+            MyClassC c = new MyClassC();
+            c.Method();
+
+            Console.ReadLine();
+        }
+    }
+}
