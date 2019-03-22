4ce7680e8f301bb0da47451ff5b6951c94e4e2ff
RyokoAzuno <ryokoazuno@gmail.com>
2019-03-22 22:16:33 +0300

    Add FindNextBiggerNumber and Tests

--- NET.S.2019.Kazimirau.02/Tasks.Day02/Tasks.cs
+++ NET.S.2019.Kazimirau.02/Tasks.Day02/Tasks.cs
@@ -5,6 +5,7 @@ namespace Tasks.Day02
 {
     public static class Tasks
     {
+        #region InsertNumber
         public static int InsertNumber(int a, int b, int i, int j)
         {
             string result = string.Empty;
@@ -15,5 +16,43 @@ namespace Tasks.Day02
 
             return Convert.ToInt32(result, 2);
         }
+        #endregion
+
+        #region FindNextBiggerNumber
+        public static int FindNextBiggerNumber(int n)
+        {
+            string str = n.ToString();
+            int tmp = n + 1;
+            while (tmp < int.MaxValue)
+            {
+                string s = tmp.ToString();
+                if (str.Length < s.Length)
+                    break;
+                if (s.Length == str.Length)
+                {
+                    if (AreEqual(str, s))
+                    {
+                        return Convert.ToInt32(s);
+                    }
+                }
+                tmp++;
+            }
+            return -1;
+        }
+
+        private static bool AreEqual(string str1, string str2)
+        {
+            char[] arr1 = str1.ToArray();
+            char[] arr2 = str2.ToArray();
+            Array.Sort(arr1);
+            Array.Sort(arr2);
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i])
+                    return false;
+            }
+            return true;
+        }
+        #endregion
     }
 }
--- NET.S.2019.Kazimirau.02/Tasks.Day02/Tests/NUnitTests.cs
+++ NET.S.2019.Kazimirau.02/Tasks.Day02/Tests/NUnitTests.cs
@@ -24,5 +24,19 @@ namespace Tasks.Day02.Tests
             Assert.AreEqual(9, Tasks.InsertNumber(8, 15, 0, 0));
         }
         #endregion
+
+        #region FindNextBiggerNumber
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]
+        [TestCase(20, ExpectedResult = -1)]
+        public int FindNextBiggerNumber_Test(int n) => Tasks.FindNextBiggerNumber(n);
+        #endregion
     }
 }
