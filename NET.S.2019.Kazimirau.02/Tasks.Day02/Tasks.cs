da536cc52c939b5f0f688e708dcb094b290cb097
RyokoAzuno <ryokoazuno@gmail.com>
2019-03-22 22:49:55 +0300

    Add comments

--- NET.S.2019.Kazimirau.02/Tasks.Day02/Tasks.cs
+++ NET.S.2019.Kazimirau.02/Tasks.Day02/Tasks.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Tasks.Day02
@@ -6,6 +7,14 @@ namespace Tasks.Day02
     public static class Tasks
     {
         #region InsertNumber
+        /// <summary>
+        /// Insert bits from second number into first number
+        /// </summary>
+        /// <param name="a"> a first number </param>
+        /// <param name="b"> a second number </param>
+        /// <param name="i"> a first bit index</param>
+        /// <param name="j"> a second bit index </param>
+        /// <returns> new integer number </returns>
         public static int InsertNumber(int a, int b, int i, int j)
         {
             string result = string.Empty;
@@ -19,6 +28,11 @@ namespace Tasks.Day02
         #endregion
 
         #region FindNextBiggerNumber
+        /// <summary>
+        /// Find next bigger number consists from n-argument digits
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns> -1 - if there is no such number and integer value vice versa</returns>
         public static int FindNextBiggerNumber(int n)
         {
             string str = n.ToString();
@@ -39,7 +53,12 @@ namespace Tasks.Day02
             }
             return -1;
         }
-
+        /// <summary>
+        /// Check if any two strings are absolutely equivalent
+        /// </summary>
+        /// <param name="str1"> first string </param>
+        /// <param name="str2"> second string </param>
+        /// <returns> are they equal? </returns>
         private static bool AreEqual(string str1, string str2)
         {
             char[] arr1 = str1.ToArray();
@@ -52,6 +71,55 @@ namespace Tasks.Day02
                     return false;
             }
             return true;
+        }
+        #endregion
+
+        #region FilterDigit
+        /// <summary>
+        /// Filter elements of the array where arr[i] contains number
+        /// </summary>
+        /// <param name="arr"> array of ints</param>
+        /// <param name="number"> a value-filter </param>
+        /// <returns> new int array where each element consists of number value </returns>
+        public static int[] FilterDigit(int[] arr, int number)
+        {
+            string[] strs = IntArrayToStringArray(arr);
+            List<string> lst = new List<string>();
+            int count = 0;
+            for (int i = 0; i < strs.Length; i++)
+            {
+                foreach (var item in strs[i])
+                {
+                    if (item == number.ToString()[0])
+                    {
+                        lst.Add(strs[i]);
+                        ++count;
+                        break;
+                    }
+                }
+            }
+            int[] result = new int[count];
+            for (int i = 0; i < lst.Count; i++)
+            {
+                result[i] = Convert.ToInt32(lst[i]);
+            }
+
+            return result;
+        }
+        /// <summary>
+        /// Convert array of integers into array of strings
+        /// </summary>
+        /// <param name="arr"> array of ints </param>
+        /// <returns> array of strings </returns>
+        private static string[] IntArrayToStringArray(int[] arr)
+        {
+            string[] str = new string[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str[i] = arr[i].ToString();
+            }
+
+            return str;
         }
         #endregion
     }
