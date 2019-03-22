8e3a4a69e7cfcc32d302865dd774b2909706721b
RyokoAzuno <ryokoazuno@gmail.com>
2019-03-22 21:20:23 +0300

    Add InsertNumber method

---
+++ NET.S.2019.Kazimirau.02/Tasks.Day02/Tasks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Tasks.Day02
+{
+    public static class Tasks
+    {
+        public static int InsertNumber(int a, int b, int i, int j)
+        {
+            string result = string.Empty;
+            string numA = new string(Convert.ToString(a, 2).PadLeft(32, '0').Reverse().ToArray());
+            string numB = new string(Convert.ToString(b, 2).PadLeft(32, '0').Reverse().ToArray());
+            string bitsToInsert = numB.Substring(0, j - i + 1);
+            result = new string(numA.Remove(i, j - i + 1).Insert(i, bitsToInsert).Reverse().ToArray());
+
+            return Convert.ToInt32(result, 2);
+        }
+    }
+}
---
+++ NET.S.2019.Kazimirau.02/Tasks.Day02/Tests/NUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Tasks.Day02.Tests
+{
+    [TestFixture]
+    public class NUnitTests
+    {
+        #region InsertNumberTests
+        [Test]
+        public void InsertNumber_Test1()
+        {
+            Assert.AreEqual(120, Tasks.InsertNumber(8, 15, 3, 8));
+        }
+
+        [Test]
+        public void InsertNumber_Test2()
+        {
+            Assert.AreEqual(15, Tasks.InsertNumber(15, 15, 0, 0));
+        }
+
+        [Test]
+        public void InsertNumber_Test3()
+        {
+            Assert.AreEqual(9, Tasks.InsertNumber(8, 15, 0, 0));
+        }
+        #endregion
+    }
+}
