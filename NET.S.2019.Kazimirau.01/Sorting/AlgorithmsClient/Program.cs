681d808f1bf47c5052fa35d8c0043fe71841e1b8
RyokoAzuno <ryokoazuno@gmail.com>
2019-03-24 12:16:45 +0300

    Add new comments

--- NET.S.2019.Kazimirau.01/Sorting/AlgorithmsClient/Program.cs
+++ NET.S.2019.Kazimirau.01/Sorting/AlgorithmsClient/Program.cs
@@ -13,6 +13,7 @@ namespace AlgorithmsClient
         {
             int[] array1 = { -2, 5, -1, 3, 1, 2, -3, 4, 0 };
             int[] array2 = { -8, 5, -1, 33, 81, -2, -3, 4, 0 };
+            //SortingContext sortingContext = new SortingContext(new QuickSortAlgorithm(SortingAlgorithm.CreateRandomArray(20000000))); // ~6000 ms
             SortingContext sortingContext = new SortingContext(new QuickSortAlgorithm(array1));
             Stopwatch sw = new Stopwatch();
 
@@ -28,6 +29,7 @@ namespace AlgorithmsClient
             Console.WriteLine();
 
             Console.WriteLine("Mergesort:");
+            //sortingContext.SetAlgorithm(new MergeSortAlgorithm(SortingAlgorithm.CreateRandomArray(20000000))); // ~11000 ms
             sortingContext.SetAlgorithm(new MergeSortAlgorithm(array2));
 
             sw.Start();
