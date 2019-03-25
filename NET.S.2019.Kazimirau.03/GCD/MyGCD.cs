8e26f445bfb838e48b3610fd503396c879256867
RyokoAzuno <ryokoazuno@gmail.com>
2019-03-25 23:20:48 +0300

    Add Euclidean and binary Gcd  methods

---
+++ NET.S.2019.Kazimirau.03/GCD/MyGCD.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace GCD
+{
+    public class MyGCD
+    {
+        #region BinaryGcd
+        /// <summary>
+        /// Binary GCD(Stein's algorithm) algorithm: An alternative method of computing the gcd, method which uses only
+        /// subtraction and division by 2 (gcd(a, b) = a * 2^d).
+        /// BinaryGCD: https://en.wikipedia.org/wiki/Greatest_common_divisor
+        /// </summary>
+        /// <param name="a"> Number a </param>
+        /// <param name="b"> Number b </param>
+        /// <returns> Greatest common divisor </returns>
+        public static int BinaryGcd(int a, int b)
+        {
+            if (a == 0 && b == 0)
+                throw new ArgumentException();
+
+            if (a == 0 && b > 0)
+                return b;
+            else if (b == 0 && a > 0)
+                return a;
+
+            if (a < 0)
+            {
+                a = Math.Abs(a);
+                if (b == 0)
+                    return a;
+            }
+            if (b < 0)
+            {
+                b = Math.Abs(b);
+                if (a == 0)
+                    return b;
+            }
+
+            var elts = Binary(a, b);
+            int result = elts.Item1 * Convert.ToInt32(Math.Pow(2, elts.Item2));
+
+            return result;
+        }
+        ///
+        private static (int, int) Binary(int a, int b)
+        {
+            int g = 0;                      // keeper for the first number(a)
+            int d = 0;                      // records the number of times that a and b have been both divided by 2
+            if (a > 0 && b > 0)
+            {
+                // Both a and b are even.
+                // In this case 2 is a common divisor. Divide both a and b by 2, increment d by 1
+                // to record the number of times 2 is a common divisor and continue.
+                while ((a % 2 == 0) && (b % 2 == 0))
+                {
+                    a = a / 2;
+                    b = b / 2;
+                    ++d;
+                }
+                while (a != b)
+                {
+                    if (a % 2 == 0)         // In this case 2 is not a common divisor. Divide a by 2 and continue.
+                        a = a / 2;
+                    else if (b % 2 == 0)    // As in the previous case 2 is not a common divisor. Divide b by 2 and continue.
+                        b = b / 2;
+                    else if (a > b)         // As gcd(a,b) = gcd(b,a) and consider that a = b, we may assume that a > b.
+                        a = (a - b) / 2;    // Each of the above steps reduces at least one of a and b towards 0 and so can only 
+                    else                    // be repeated a finite number of times. 
+                        b = (b - a) / 2;    // Thus one must eventually reach the case a = b, which is the only stopping case.
+                }
+                g = a;
+            }
+
+            return (g, d);
+        }
+        #endregion
+
+        #region EuclideanGcd
+        /// <summary>
+        ///  Euclidean algorithm, is a method for computing the greatest common divisor (GCD) of two numbers.
+        ///  Euclidean algorithm: https://en.wikipedia.org/wiki/Euclidean_algorithm
+        /// </summary>
+        /// <param name="a"> Number a</param>
+        /// <param name="b"> Number b</param>
+        /// <returns> Greatest common divisor </returns>
+        public static int EuclideanGcd(int a, int b)
+        {
+            if (a == 0 && b == 0)
+                throw new ArgumentException();
+
+            if (a == 0 && b > 0)
+                return b;
+            else if (b == 0 && a > 0)
+                return a;
+
+            if (a < 0)
+            {
+                a = Math.Abs(a);
+                if (b == 0)
+                    return a;
+            }
+            if (b < 0)
+            {
+                b = Math.Abs(b);
+                if (a == 0)
+                    return b;
+            }
+
+            // Euclidean Algorithm
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+
+            return a;
+        }
+
+        public static int EuclideanGcd(params int[] nums)
+        {
+            if (nums.Length < 2)
+                throw new ArgumentException();
+
+            int result = EuclideanGcd(nums[0], nums[1]);
+
+            if (nums.Length > 2)
+            {
+                for (int i = 2; i < nums.Length; i++)
+                    result = EuclideanGcd(result, nums[i]);
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ NET.S.2019.Kazimirau.03/GCD/Tests/NUnitTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace GCD.Tests
+{
+    [TestFixture]
+    public class NUnitTests
+    {
+        #region BinaryGcd
+        [TestCase(30, 12, ExpectedResult = 6)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(8, 9, ExpectedResult = 1)]
+        [TestCase(50, 250, ExpectedResult = 50)]
+        [TestCase(10927782, -6902514, ExpectedResult = 846)]
+        [TestCase(-10234567, -234568989, ExpectedResult = 97)]
+        [TestCase(0, -301, ExpectedResult = 301)]
+        [TestCase(-1590771464, 0, ExpectedResult = 1590771464)]
+        [TestCase(10927782, 0, ExpectedResult = 10927782)]
+        public int BinaryGcdTests(int a, int b) => MyGCD.BinaryGcd(a, b);
+
+        [Test]
+        public void BinaryGcdTest_WithTwoZeroNumbers_ThrowArgumentException()
+        {
+            Assert.Throws<System.ArgumentException>(() => MyGCD.BinaryGcd(0, 0));
+        }
+        #endregion
+
+        #region EuclideanGcd
+        [TestCase(30, 12, ExpectedResult = 6)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(8, 9, ExpectedResult = 1)]
+        [TestCase(50, 250, ExpectedResult = 50)]
+        [TestCase(10927782, -6902514, ExpectedResult = 846)]
+        [TestCase(-10234567, -234568989, ExpectedResult = 97)]
+        [TestCase(0, -301, ExpectedResult = 301)]
+        [TestCase(-1590771464, 0, ExpectedResult = 1590771464)]
+        [TestCase(10927782, 0, ExpectedResult = 10927782)]
+        public int EuclideanGcdTests(int a, int b) => MyGCD.EuclideanGcd(a, b);
+
+        [Test]
+        public void EuclideanGcdTest_WithTwoZeroNumbers_ThrowArgumentException()
+        {
+            Assert.Throws<System.ArgumentException>(() => MyGCD.EuclideanGcd(0, 0));
+        }
+        #endregion
+
+        [TestCase(85886, 94344, 73964, 42448, ExpectedResult = 2)]
+        [TestCase(85884, 94344, 73464, 42448, ExpectedResult = 4)]
+        [TestCase(85888, 94848, 73464, 42448, 98888, ExpectedResult = 8)]
+        public int EuclideanGcd_WithParams_Tests(params int[] arr) => MyGCD.EuclideanGcd(arr);
+    }
+}
