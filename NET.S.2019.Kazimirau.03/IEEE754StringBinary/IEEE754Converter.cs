e91f6efebd4089e2b6809468ffd3a5f775b1c5c1
RyokoAzuno <ryokoazuno@gmail.com>
2019-03-28 15:29:55 +0300

    Add IEEE754Converter some fixes

--- NET.S.2019.Kazimirau.03/IEEE754StringBinary/IEEE754Converter.cs
+++ NET.S.2019.Kazimirau.03/IEEE754StringBinary/IEEE754Converter.cs
@@ -8,40 +8,64 @@ namespace IEEE754StringBinary
     {
         public static string Convert(double number)
         {
-            //string result = string.Empty;
-            //char sign = number >= 0 ? '0' : '1';
-            //string exp = GetExponenta(number);
-            //string whole = GetWholePart(number);
-            //string frac = GetFractionalPart(number);
-            //result = $"{sign}{exp}{whole.Substring(1)}{frac}";
-            //int length = result.Length;
-            //if (length < 64)
-            //    result = result.PadRight(64, '0');
-            //return result;
+            string result = string.Empty;
 
-            byte[] numberBytes = BitConverter.GetBytes(number);
-            BitArray bitArray = new BitArray(numberBytes);
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = bitArray.Length - 1; i >= 0; i--)
+            if (double.IsNegativeInfinity(number))
             {
-                if (bitArray[i])
-                    sb.Append('1');
-                else
-                    sb.Append('0');
+                return "1111111111110000000000000000000000000000000000000000000000000000";
             }
+            else if(double.IsPositiveInfinity(number))
+            {
+                return "0111111111110000000000000000000000000000000000000000000000000000";
+            }
+            else if(double.IsNaN(number))
+            {
+                return "1111111111111000000000000000000000000000000000000000000000000000";
+            }
+            
+            char sign = number >= 0 ? '0' : '1';            
+            string exponent = GetExponenta(number);
+            string mantissa = GetMantissa(number);
+            result = $"{sign}{exponent}{mantissa}";
+            int length = result.Length;
 
-            return sb.ToString();
+            if (length < 64)
+                result = result.PadRight(64, '0');
+            return result;
+
+            //byte[] numberBytes = BitConverter.GetBytes(number);
+            //BitArray bitArray = new BitArray(numberBytes);
+            //StringBuilder sb = new StringBuilder();
+
+            //for (int i = bitArray.Length - 1; i >= 0; i--)
+            //{
+            //    if (bitArray[i])
+            //        sb.Append('1');
+            //    else
+            //        sb.Append('0');
+            //}
+
+            //return sb.ToString();
+        }
+        private static string GetMantissa(double number)
+        {
+            string wholePart = GetWholePart(number);
+            string fractPart = GetFractionalPart(number);
+
+            string result = string.IsNullOrEmpty(wholePart) == true ? fractPart : wholePart.Substring(1) + fractPart;
+            return result.Length == 52 ? result : result.Length < 52 ? result.PadRight(52, '0') : result.Substring(result.Length - 52);
         }
 
-        public static string GetWholePart(double number)
+        private static string GetWholePart(double number, int numBits = 52)
         {
-            StringBuilder sb = new StringBuilder();
-            long whole = (long)number;
+            StringBuilder sb = new StringBuilder(numBits);
+
+            long whole = (long)Math.Abs(number);
+            long reminder = 0;
 
             while(whole != 0)
             {
-                long reminder = whole % 2;
+                reminder = whole % 2;
                 whole /= 2;
                 sb.Append(reminder);
             }
@@ -50,9 +74,9 @@ namespace IEEE754StringBinary
             return sb.ToString();
         }
 
-        public static string GetFractionalPart(double number)
+        private static string GetFractionalPart(double number)
         {
-            double fractional = number % 1;
+            double fractional = Math.Abs(number) % 1;                       
             StringBuilder sb = new StringBuilder();
 
             while(fractional != 0)
@@ -74,16 +98,20 @@ namespace IEEE754StringBinary
             return sb.ToString();
         }
 
-        public static string GetExponenta(double number)
+        private static string GetExponenta(double number)
         {
+            string result = string.Empty;
             int power = 0;
+            number = Math.Abs(number);
             double tmp = number;
+
             if (number < 1)
             {
                 while (tmp < 2)
                 {
                     tmp = number / Math.Pow(2, --power);
                 }
+                ++power;
             }
             else
             {
@@ -92,7 +120,9 @@ namespace IEEE754StringBinary
                     tmp = number / Math.Pow(2, ++power);
                 }
             }
-            return GetWholePart(power + 1023); // 127 for single
+            //int power = wholePart.Length - 1;
+            result = GetWholePart(power + 1023); // 127 for single
+            return result.Length == 11 ? result : result.PadLeft(11, '0'); 
         }
 
         private static StringBuilder Reverse(StringBuilder sb)
