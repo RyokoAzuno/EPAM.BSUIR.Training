e06e3e6acebf217829ac5828373397fbbbdc604f
RyokoAzuno <ryokoazuno@gmail.com>
2019-04-14 11:38:04 +0300

    Add comments to BankAccount

--- NET.S.2019.Kazimirau.05/BankAccount/BankAccount.cs
+++ NET.S.2019.Kazimirau.05/BankAccount/BankAccount.cs
@@ -2,8 +2,14 @@
 
 namespace BankAccount
 {
+    /// <summary>
+    /// Represent a type of bank account
+    /// </summary>
     public enum AccountType { Base, Gold, Platinum }
 
+    /// <summary>
+    /// Simle class which represents a bank account and operations with it
+    /// </summary>
     public class BankAccount
     {
         public int Id { get; set; }
@@ -11,48 +17,78 @@ namespace BankAccount
         public decimal Balance { get; set; }
         public int Points { get; set; }
         public AccountType Type { get; set; }
+        private bool _isOpened;
 
+        // Constructor
         public BankAccount(string owner, decimal balance, int points, AccountType accType)
         {
             Id = base.GetHashCode();
             Owner = owner;
             Balance = balance;
             Points = points;
-            Type = accType; 
+            Type = accType;
+            _isOpened = true;
         }
 
+        /// <summary>
+        /// Add amount of money to bank account
+        /// </summary>
+        /// <param name="amount"> Amount of money to add </param>
         public void Deposit(decimal amount)
         {
-            Balance += amount;
-            switch (Type)
-            {
-                case AccountType.Base: Points += 5; break;
-                case AccountType.Gold: Points += 10; break;
-                case AccountType.Platinum: Points += 15; break;
-                default: break;
-            }
-        }
+            if (amount < 0)
+                throw new ArgumentException("Amount must be greater than zero!");
 
-        public void Withdraw(decimal amount)
-        {
-            if(Balance - amount >= 0)
+            if (_isOpened)
             {
-                Balance -= amount;
+                Balance += amount;
                 switch (Type)
                 {
-                    case AccountType.Base: Points -= 2; break;
-                    case AccountType.Gold: Points -= 4; break;
-                    case AccountType.Platinum: Points -= 5; break;
+                    case AccountType.Base: Points += 5; break;
+                    case AccountType.Gold: Points += 10; break;
+                    case AccountType.Platinum: Points += 15; break;
                     default: break;
                 }
             }
         }
 
-        public void Close()
+        /// <summary>
+        /// Withdraw amount of money from bank account
+        /// </summary>
+        /// <param name="amount"> Amount of money to withdraw </param>
+        public void Withdraw(decimal amount)
         {
+            if (amount < 0)
+                throw new ArgumentException("Amount must be greater than zero!");
 
+            if (_isOpened)
+            {
+                if (Balance - amount >= 0)
+                {
+                    Balance -= amount;
+                    switch (Type)
+                    {
+                        case AccountType.Base: Points -= 2; break;
+                        case AccountType.Gold: Points -= 4; break;
+                        case AccountType.Platinum: Points -= 5; break;
+                        default: break;
+                    }
+                }
+            }
         }
 
+        /// <summary>
+        /// Close a bank account
+        /// </summary>
+        public void Close()
+        {
+            _isOpened = false;
+        }
+
+        /// <summary>
+        /// Reperesent a bank object as a string
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"Id: {Id}\nOwner: {Owner}\nBalance: {Balance}\nPoints: {Points}\nType: {Type}";
