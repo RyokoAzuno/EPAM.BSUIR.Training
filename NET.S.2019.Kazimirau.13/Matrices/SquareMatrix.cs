7faba4a53d03982da45ee21df69c3e9e81f2f444
RyokoAzuno <ryokoazuno@gmail.com>
2019-04-23 14:05:15 +0300

    Add SquareMatrix class - test version

---
+++ NET.S.2019.Kazimirau.13/Matrices/SquareMatrix.cs
@@ -0,0 +1,71 @@
+﻿namespace Matrices
+{
+    public class SquareMatrix<T> where T: struct
+    {
+        private T[,] _matrix;
+
+        public SquareMatrix(int order)
+        {
+            _matrix = new T[order, order];
+        }
+
+        public SquareMatrix(int order, T[,] arr)
+        {
+            _matrix = new T[order, order];
+
+            for (int outer = arr.GetLowerBound(0); outer <= arr.GetUpperBound(0); outer++)
+            {
+                for (int inner = arr.GetLowerBound(1); inner <= arr.GetUpperBound(1); inner++)
+                {
+                    _matrix[outer, inner] = arr[outer, inner];
+                }
+            }
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                return _matrix[i, j];
+            }
+            set
+            {
+                _matrix[i, j] = value;
+            }
+        }
+
+        public SquareMatrix<T> Add(SquareMatrix<T> m)
+        {
+            int order = _matrix.GetUpperBound(0) + 1;
+            SquareMatrix<T> result = new SquareMatrix<T>(order);
+
+            for (int outer = _matrix.GetLowerBound(0); outer <= _matrix.GetUpperBound(0); outer++)
+            {
+                for (int inner = _matrix.GetLowerBound(1); inner <= _matrix.GetUpperBound(1); inner++)
+                {
+                    dynamic d1 = _matrix[outer, inner];
+                    dynamic d2 = m[outer, inner];
+                    result[outer, inner] = d1 + d2;
+                }
+            }
+
+            return result;
+        }
+
+        public T[,] ToArray()
+        {
+            int order = _matrix.GetUpperBound(0) + 1;
+            T[,] arr = new T[order, order];
+
+            for (int outer = _matrix.GetLowerBound(0); outer <= _matrix.GetUpperBound(0); outer++)
+            {
+                for (int inner = _matrix.GetLowerBound(1); inner <= _matrix.GetUpperBound(1); inner++)
+                {
+                    arr[outer, inner] = _matrix[outer, inner];
+                }
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ NET.S.2019.Kazimirau.13/Matrices/Tests/MatricesNUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Matrices.Tests
+{
+    [TestFixture]
+    public class MatricesNUnitTests
+    {
+        [Test]
+        public void Matrices_Int32_Add_Test()
+        {
+            int[,] arr1 = { { 1, 3 }, { 1, 0 } };
+            int[,] arr2 = { { 0, 0 }, { 7, 5 } };
+            SquareMatrix<int> m1 = new SquareMatrix<int>(2, arr1);
+            SquareMatrix<int> m2 = new SquareMatrix<int>(2, arr2);
+
+            SquareMatrix<int> m3 = m1.Add(m2);
+            CollectionAssert.AreEqual(new[,] { { 1, 3 }, { 8, 5 } }, m3.ToArray());
+        }
+    }
+}
